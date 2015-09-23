2009eae411d18426012b8b3b4c38262f8fdc7acf
goodkele <goodkele@gmail.com>
2015-09-23 23:15:18 +0800

    no message

---
+++ library/orm/OrmUser.go
@@ -0,0 +1,26 @@
+package orm
+
+type OrmUser struct {
+	
+	isLoad bool		// 是否已载入
+}
+
+// 存储
+func (this *OrmUser) SaveDb() {
+	
+}
+
+// 载入
+func (this *OrmUser) LoadDb() {
+	
+}
+
+// 插入
+func (this *OrmUser) Insert() {
+	
+}
+
+// 删除
+func (this *OrmUser) Delete() {
+	
+}
