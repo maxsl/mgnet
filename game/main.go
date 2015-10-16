63a158ab73a79f62695217f2ebd3310e3c6a1c84
wulei <wulei@shinezone.com>
2015-10-16 18:26:15 +0800

    protobuf

--- game/main.go
+++ game/main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/goodkele/mgnet/library/types"
+	"github.com/golang/protobuf/proto"
 )
 
 func main() {
-	fmt.Println("asdf")
+	
+//	var pro = proto.Marshal
+	
+
+	req := &types.SearchRequest{}
+	
+	//req2 := &types.SearchRequest{}
+	
+	req.Query = "asdfasdf"
+	
+	pb := proto.MarshalTextString(req)
+
+	//pb,_ := proto.MarshalMessageSetJSON(req)
+	// pb,_ := proto.Marshal(req)
+	
+	fmt.Println(pb)
+
+	//proto.Unmarshal(pb, req2)
+	//proto.unm(pb, req2)
+	
+	//fmt.Println(req2)
+	
+	
+	fmt.Println(req)
 }
